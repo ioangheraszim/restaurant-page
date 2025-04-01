3bc82f0a5b5cccbb243d1334c58609dfc6b4ada2
ioangheraszim <gijohny17@gmail.com>
2025-04-01 17:27:06 +0100

    Added menu data to contentData.js for dynamic HTML generation - Populated 'contentData.js' with data to dynamically generate the menu items

--- src/data/contentData.js
+++ src/data/contentData.js
@@ -28,3 +28,44 @@ export const cardsData = [
     buttonText: "Book Now",
   },
 ];
+
+export const menuData = [
+  {
+    title: "FIRST",
+    subtitle: "SMALL PIECES, IDEAL FOR SHARING",
+    items: [
+      { dish: "Oyster – shaved scallop, sea herbs, lemon (GF)", price: 4.5 },
+      { dish: "Mill house bread – smoked cultured butter (V)", price: 5 },
+      { dish: "7 spiced pepita seeds – orange, garlic, capsicum, pork (GF)", price: 5 },
+      { dish: "Tasmanian salmon – ginger, kaffir, finger lime, grape (GF)", price: 5 },
+      { dish: "Chicken liver pate – spiced bread, crackling", price: 8 },
+      { dish: "Scallop cannelloni – nettle, bower spinach, katsuobushi", price: 9 },
+      { dish: "Kangaroo tartare – green olive, miso, flax cracker (GF)", price: 12 },
+    ],
+  },
+  {
+    title: "MAIN",
+    subtitle: "LARGER SINGLE-SERVE DISHES",
+    items: [
+      { dish: "Arcadia Lamb - cucumber, lemon, honey, sake (GF)", price: 4.5 },
+      { dish: "Roasted duck - pickled turnip, black garlic, slow-cooked grains", price: 5 },
+      { dish: "Smoked Abalone - pearl oyster, heart of palm, black vinegar (GF)", price: 5 },
+      { dish: "Pickled Quail - walnut crackling, cured seaweed, star anise", price: 5 },
+      { dish: "Confit Salmon - shiitake, grains, horseradish emulsion", price: 8 },
+      { dish: "Slow Cooked Cod - eggplant, hazelnut, juniper berry (GF)", price: 9 },
+      { dish: "Pickled Mussels– nettle, bower spinach, katsuobushi", price: 12 },
+    ],
+  },
+  {
+    title: "DESSERT",
+    subtitle: "SMALL SERVINGS, IDEAL FOR SHARING",
+    items: [
+      { dish: "Lychee tart – filtered créme fràiche, blueberries (V)", price: 12 },
+      { dish: "Caramel cheesecake – apple leaves, ginger (V)", price: 5 },
+      { dish: "Sweet potato ice cream – shaved pear, cacao (V) (GF)", price: 5 },
+      { dish: "Pickled Quail - walnut crackling, cured seaweed, star anise", price: 5 },
+      { dish: "Chocolate friand – quince paste, ginger root (V)", price: 8 },
+    ],
+  },
+];
+
